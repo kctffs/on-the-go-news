260e4bbaeb19d0436c3060bd7d2864c7a90d7ab9
Kieran Cattermole <kctffs@gmail.com>
2024-09-29 01:11:59 +0000

    Added the post model for news app

--- onthegonews/settings.py
+++ onthegonews/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dq1g)daf&dmxc9tiivy@1y4r&_w67tga56wf4+k4t2=!g_d##7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kctffs-onthegonews-7dsakknu0qq.ws-eu116.gitpod.io',
                 '.herokuapp.com']
@@ -91,7 +91,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+    "https://8000-kctffs-onthegonews-7dsakknu0qq.ws-eu116.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
